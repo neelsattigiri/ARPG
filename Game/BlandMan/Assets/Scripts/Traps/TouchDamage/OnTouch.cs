bcf6ecca6ab96d5d1c8eff6bae4e8b5b16029b18
Neel Sattigiri <neelsattigiri@gmail.com>
2024-04-21 01:47:52 +0530

    Moved OnTouch.cs to its own folder.
    -Moved OnTouch.cs from "\Assets " to "\Assets\Scripts\Traps\TouchDamage".
    -Created empty scene "Test_FlyingEnemy" to test flying enemies.

---
+++ Game/BlandMan/Assets/Scripts/Traps/TouchDamage/OnTouch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouch : MonoBehaviour
+{
+
+    // Public variables
+    // Damage received by player when touching this object
+    public int touchDmg = 1;
+    // Magnitude of knockback in X direction for the player when touching this object
+    public int forcedMovementX = 1;
+    // Magnitude of knockback in y direction for the player when touching this object
+    public int forcedMovementY = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
