19735d38479d8772612fc81a046b19e3ba320a45
Neel Sattigiri <neelsattigiri@gmail.com>
2024-04-21 02:56:18 +0530

    Added First Flying enemy
    -Added "EnemyBat" in scene "Test_FlyingEnemy"
    -Added Basic chase function for EnemyBat
    -Added placeholder flying animation for EnemyBat

---
+++ Game/BlandMan/Assets/Scripts/Enemies/EnemyBat/EnemyBatAI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBatAI : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+        Chase();
+        Flip();
+    }
+
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
